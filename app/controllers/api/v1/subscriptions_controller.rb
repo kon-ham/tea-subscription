d7f83f8e101ba3da9c44dd22cff26760e641aa14
ignored-comment <info.konham@gmail.com>
2021-09-22 13:30:40 -0400

    Create endpoints for tea-subscriptions

---
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::SubscriptionsController < ApplicationController
+    def index
+        render json: {  }, status: :ok
+    end
+
+    def create
+        render json: {  }, status: :ok
+    end
+
+    def update
+        render json: {  }, status: :ok
+    end
+end
--- spec/requests/api/v1/subscription/subscription_spec.rb
+++ spec/requests/api/v1/subscription/subscription_spec.rb
@@ -1,21 +1,40 @@
 require "rails_helper"
 
-RSpec.describe "POST subscriptions" do
+RSpec.describe "Subscriptions" do
     describe 'happy path' do
-        it 'can create a session when a user logs in' do
-            headers = {
-                'Content-Type' => "application/json",
-                'Accept' => "application/json"
-            }
+        it 'POST can create a subscription' do
             body = {
-                "title": "entry",
-                "price": "15.99",
-                "status": "shipped",
-                "frequency": "1"
+                title: "entry",
+                price: "15.99",
+                status: "1",
+                frequency: "1",
+                email: "bob@bob.com"
             }
-            post '/api/v1/subscriptions', headers: headers, params: body.to_json
-            session = JSON.parse(response.body, symbolize_names: true)
+            post '/api/v1/subscriptions', params: body
+            subscription = JSON.parse(response.body, symbolize_names: true)
 
+            expect(response).to be_successful
+            expect(response.status).to eq(200)
+        end
+
+        it "PATCH can cancel a customer's subscription" do
+            body = {
+                status: "0"
+            }
+            patch "/api/v1/subscriptions/:id", params: body
+            subscription = JSON.parse(response.body, symbolize_names: true)
+
+            expect(response).to be_successful
+            expect(response.status).to eq(200)
+        end
+
+        it "GET can get all of a customer's subscription" do
+            body = {
+                email: "bob@bob.com"
+            }
+            get '/api/v1/subscriptions/', params: body
+            subscription = JSON.parse(response.body, symbolize_names: true)
+            
             expect(response).to be_successful
             expect(response.status).to eq(200)
         end
