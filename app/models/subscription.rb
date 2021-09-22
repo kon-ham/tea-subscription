71732d63906f2ffbd6632b4daa122e25977080ff
ignored-comment <info.konham@gmail.com>
2021-09-22 14:55:09 -0400

    Pass validation tests for customer and subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,8 @@
 class Subscription < ApplicationRecord
   belongs_to :customer
+
+  validates :title, presence: true
+  validates :price, presence: true, numericality: true
+  validates :status, presence: true, numericality: true
+  validates :frequency, presence: true, numericality: true
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:customer) }
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:price) }
+  it { should validate_presence_of(:status) }
+  it { should validate_presence_of(:frequency) }
+
+  it { should validate_numericality_of(:price) }
+  it { should validate_numericality_of(:status) }
+  it { should validate_numericality_of(:frequency) }
 end
