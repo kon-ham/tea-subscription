c6b190a8c987742655867fdce9b38a52638c8c94
ignored-comment <info.konham@gmail.com>
2021-09-22 16:20:22 -0400

    Pass happy and sad path testing for POST subscriptions

--- spec/requests/api/v1/subscription/subscription_spec.rb
+++ spec/requests/api/v1/subscription/subscription_spec.rb
@@ -1,42 +1,81 @@
 require "rails_helper"
 
 RSpec.describe "Subscriptions" do
-    describe 'happy path' do
-        it 'POST can create a subscription' do
+    before :each do 
+        @customer = Customer.create(
+            first_name: "Bob", 
+            last_name: "McBobster",
+            email: "bob@mcbobster.com", 
+            address: "123 ABC St, Charlotte NC, 28226"
+        )
+    end
+
+    describe 'endpoint creation' do
+        it 'POST subscription endpoint exists' do
             body = {
                 title: "entry",
                 price: "15.99",
                 status: "1",
                 frequency: "1",
-                email: "bob@bob.com"
+                email: "bob@mcbobster.com"
             }
             post '/api/v1/subscriptions', params: body
-            subscription = JSON.parse(response.body, symbolize_names: true)
-
             expect(response).to be_successful
             expect(response.status).to eq(200)
         end
 
-        it "PATCH can cancel a customer's subscription" do
+        it "PATCH can cancel a customer's subscription endpoint exists" do
             body = {
                 status: "0"
             }
             patch "/api/v1/subscriptions/:id", params: body
-            subscription = JSON.parse(response.body, symbolize_names: true)
 
             expect(response).to be_successful
             expect(response.status).to eq(200)
         end
 
-        it "GET can get all of a customer's subscription" do
+        it "GET can get all of a customer's subscription endpoint exists" do
             body = {
                 email: "bob@bob.com"
             }
             get '/api/v1/subscriptions/', params: body
-            subscription = JSON.parse(response.body, symbolize_names: true)
             
             expect(response).to be_successful
             expect(response.status).to eq(200)
+        end
+    end
+
+    describe 'Create a subscription' do
+        it 'HAPPY PATH it can create a new subscription for a customer' do
+            body = {
+                title: "entry",
+                price: "15.99",
+                status: "1",
+                frequency: "1",
+                email: "bob@mcbobster.com"
+            }
+            post '/api/v1/subscriptions', params: body
+            subscription = JSON.parse(response.body, symbolize_names: true)
+
+            expect(subscription[:message]).to eq("Subscription has been created")
+            expect(response).to be_successful
+            expect(response.status).to eq(200)
+        end
+
+        it 'SAD PATH it cannot create a new subscription for a customer' do
+            body = {
+                title: "",
+                price: "15.99",
+                status: "1",
+                frequency: "1",
+                email: "bob@mcbobster.com"
+            }
+            post '/api/v1/subscriptions', params: body
+            subscription = JSON.parse(response.body, symbolize_names: true)
+
+            expect(subscription[:errors]).to eq("Field is blank or incorrect data was given for fields")
+            expect(response).to_not be_successful
+            expect(response.status).to eq(400)
         end
     end
 end
